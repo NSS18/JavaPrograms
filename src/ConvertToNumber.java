7815788a30094b394b171c7fc313e60ad93c3662
sapan shah <nidhisoftwaretesting@gmail.com>
2019-12-16 10:08:59 +0000

    homework done

--- src/ConvertToNumber.java
+++ src/ConvertToNumber.java
@@ -1,6 +1,22 @@
 public class ConvertToNumber {
-    public static void main(String[] args) {
-        
+
+        //method to convert string into number
+
+        public static void StringToNumber(String price1, String price2) {
+            int total;
+            total = Integer.parseInt(price1) + Integer.parseInt(price2);
+            System.out.println("total of " + price1 + " and " + price2 + " is: " + total);
+        }
+
+        //main method
+        //code review applied
+        public static void main(String[] args) {
+
+            StringToNumber("100", "200");       //calling StringToNumber method using string as parameter
+            StringToNumber("150", "200");
+
+        }
+
     }
-}
+
 
